52d1371d2a770f37d8a25ed1fabe2edf73d5aec4
minju <minju9711@naver.com>
2024-10-05 22:37:18 +0900

    [add] 일대다 테이블 생성(예제 코드에선 주인이 아닌 쪽에 @JoinColumn을 붙이도록 되어있지만, 실제 돌려보면 jpa에서 해당 기능을 막아놨음)

---
+++ src/main/java/chap_6/OneToMany/Chap6_OTM_Member.java
@@ -0,0 +1,33 @@
+package chap_6.OneToMany;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Chap6_OTM_Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String username;
+
+    //여기까진 단방향 설정
+    // ----------------------------------
+    //여기서부턴 양방향 설정
+
+    @ManyToOne
+    @JoinColumn(name="TEAM_ID")
+    private Chap6_OTM_Team team; //"다대일" 관계이기에 외래키 관리를 "다"에서 한다.
+
+//    public void setTeam(Chap6_OTM_Team team){
+//        this.team = team;
+//
+//        //무한루프에 빠지지 않도록 체크
+//        if(!team.getMembers().contains(this)) {
+//            team.getMembers().add(this);
+//        }
+//    }
+
+}
---
+++ src/test/java/chap_6/ManyToOne/Chap6_MTO_Test.java
@@ -0,0 +1,7 @@
+package chap_6.ManyToOne;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Chap6_MTO_Test {
+
+}
