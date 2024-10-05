ec9615955db937323ff59e78581d2fc9bc8dd2d8
minju <minju9711@naver.com>
2024-10-06 02:59:46 +0900

    [test] : 다대일 엔티티 테스트용 클래스 추가

--- src/test/java/chap_6/ManyToOne/Chap6_MTO_Test.java
+++ src/test/java/chap_6/ManyToOne/Chap6_MTO_Test.java
@@ -1,7 +1,11 @@
 package chap_6.ManyToOne;
 
-import static org.junit.jupiter.api.Assertions.*;
+import example.JpaApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 
+
+
+@SpringBootTest(classes = JpaApplication.class)
 class Chap6_MTO_Test {
 
 }
