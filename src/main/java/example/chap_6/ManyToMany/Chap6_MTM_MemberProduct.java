4e2960b7be5925b6ad4849470abfb0b2e2cbbd73
minju <minju9711@naver.com>
2024-10-06 05:06:49 +0900

    [ADD] : 다대다 중간 엔티티 생성

---
+++ src/main/java/example/chap_6/ManyToMany/Chap6_MTM_MemberProduct.java
@@ -0,0 +1,29 @@
+package example.chap_6.ManyToMany;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Chap6_MTM_MemberProduct {
+
+    //기본키를 매핑하는 @Id와 외래 키를 매핑하는 @JoinColum을 동시에 사용해서 기본키 + 외래키를 한 번에 매핑했다.
+    //또한 @IdClass를 사용해서 복합 기본키를 매핑했다.
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Chap6_MTM_Member member;
+    //MemberProductId.member와 연결
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Chap6_MTM_PRODUCT product;
+    //MemberProductId.product와 연결
+
+    private int orderAmount;
+}
