0b53e4dea27f0d609f424250e1fd8bbf47036228
minju <minju9711@naver.com>
2024-10-06 02:44:22 +0900

    [add] 일대일 매핑 엔티티 Locker 추가

---
+++ src/main/java/example/chap_6/OneToOne/Chap6_OTO_Locker.java
@@ -0,0 +1,19 @@
+package example.chap_6.OneToOne;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Chap6_OTO_Locker { //대상 테이블
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="LOCKER_ID")
+    private Long id;
+
+    private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private Chap6_OTO_Member member;
+    // 일대일 관계 중 대상 테이블에 외래키가 있는 단방향 관계는 JPA에서 지원하지 않는다.
+    // 방법1 : 단방향 관계를 현재(member->locker)에서 (locekr->member)로 수정
+    // 방법2 : 양방향 관계로 만들고 Locker를 연관관계의 주인으로 설정해야 함.
+}
