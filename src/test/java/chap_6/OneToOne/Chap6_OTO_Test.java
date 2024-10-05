5b74524756fb6711f26cf997d20041c7c041299f
minju <minju9711@naver.com>
2024-10-06 02:59:20 +0900

    [add] : 일대일 엔티티 테스트용 클래스 추가

---
+++ src/test/java/chap_6/OneToOne/Chap6_OTO_Test.java
@@ -0,0 +1,11 @@
+package chap_6.OneToOne;
+
+import example.JpaApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest(classes = JpaApplication.class)
+@Transactional
+class Chap6_OTO_Test {
+
+}
