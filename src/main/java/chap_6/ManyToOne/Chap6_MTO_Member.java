1fe740a53618462fe47df20670ffa8bf57460037
minju <minju9711@naver.com>
2024-10-05 21:42:13 +0900

    [add] 다대일 단방향, 양방향 코드 및 정리

--- src/main/java/chap_6/ManyToOne/Chap6_MTO_Member.java
+++ src/main/java/chap_6/ManyToOne/Chap6_MTO_Member.java
@@ -2,9 +2,10 @@ package chap_6.ManyToOne;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 
 @Entity
-@Data
+@Getter
 public class Chap6_MTO_Member {
 
     @Id @GeneratedValue
@@ -15,7 +16,18 @@ public class Chap6_MTO_Member {
 
     @ManyToOne
     @JoinColumn(name="TEAM_ID")
-    private Chap6_MTO_Team team;
+    private Chap6_MTO_Team team; //"다대일" 관계이기에 외래키 관리를 "다"에서 한다.
 
+    //여기까진 단방향 설정
+    // ----------------------------------
+    //여기서부턴 양방향 설정
+    public void setTeam(Chap6_MTO_Team team){
+        this.team = team;
+
+        //무한루프에 빠지지 않도록 체크
+        if(!team.getMembers().contains(this)) {
+            team.getMembers().add(this);
+        }
+    }
 
 }
--- src/main/java/chap_6/ManyToOne/Chap6_MTO_Team.java
+++ src/main/java/chap_6/ManyToOne/Chap6_MTO_Team.java
@@ -1,10 +1,10 @@
 package chap_6.ManyToOne;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -15,4 +15,21 @@ public class Chap6_MTO_Team {
     private Long id;
 
     private String name;
+
+    //단방향 일 때는 team 엔티티에 member 참조하는 필드 없음
+
+    //여기까진 단방향 설정
+    // ----------------------------------
+    //여기서부턴 양방향 설정
+
+    @OneToMany(mappedBy = "team")
+    private List<Chap6_MTO_Member> members = new ArrayList<>();
+    //주인이 아닌 Team.members는 조회를 위한 jpql이나 객체 그래프를 탐색할 때 사용
+
+    public void addMembers(Chap6_MTO_Member member){
+        this.members.add(member);
+        if(member.getTeam() != this) { //편의 메소드를 양쪽에 다 작성했을 시, 무한 루프에 빠지지 않도록 체크
+            member.setTeam(this);      //예제 코드에선 양쪽에 다 작성했는데, 실제에선 그냥 둘 중 하나에만 만들어두면 됨.
+        }
+    }
 }
