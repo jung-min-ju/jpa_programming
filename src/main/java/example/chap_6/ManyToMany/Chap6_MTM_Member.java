470567deb0a78ac0fbb90849d43002743e3146e6
minju <minju9711@naver.com>
2024-10-06 05:06:31 +0900

    [ADD] : 다대다 MEMBER 엔티티 생성

---
+++ src/main/java/example/chap_6/ManyToMany/Chap6_MTM_Member.java
@@ -0,0 +1,39 @@
+package example.chap_6.ManyToMany;
+
+import example.chap_6.OneToOne.Chap6_OTO_Locker;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Chap6_MTM_Member {
+
+    @Id
+    @Column(name="member_id")
+    private String id;
+
+    private String username;
+
+//    @ManyToMany
+//    @JoinTable(name = "Chap6_MEMBER_PRODUCT",
+//               joinColumns = @JoinColumn(name = "member_id"),
+//               inverseJoinColumns = @JoinColumn(name="product_id"))
+//    private List<Chap6_MTM_PRODUCT> products = new ArrayList<>();
+
+
+    //여기까진 중간엔티티 미사용
+    // ----------------------------------
+    //여기서부턴 중간엔티티 사용
+
+    @OneToMany(mappedBy = "member")
+    private List<Chap6_MTM_MemberProduct> memberProducts = new ArrayList<>();
+
+    public void addProducts(Chap6_MTM_PRODUCT product){
+        //products.add(product);
+        product.getMembers().add(this);
+    }
+
+}
