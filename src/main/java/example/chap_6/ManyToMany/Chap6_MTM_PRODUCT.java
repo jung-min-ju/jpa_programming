631e02545e7a9e5eed8ba686b3680afcff3986b0
minju <minju9711@naver.com>
2024-10-06 05:32:33 +0900

    [chore] : 다대다 중간테이블 테스트 위해 주석처리

--- src/main/java/example/chap_6/ManyToMany/Chap6_MTM_PRODUCT.java
+++ src/main/java/example/chap_6/ManyToMany/Chap6_MTM_PRODUCT.java
@@ -16,8 +16,13 @@ public class Chap6_MTM_PRODUCT {
 
     private String name;
 
-    @ManyToMany(mappedBy = "products" ) //역방향 추가
-    private List<Chap6_MTM_Member> members = new ArrayList<>();
+
+    //중간엔티티 미사용 -> 주석해제
+    // ----------------------------------
+    //중간엔티티 사용 -> 주석처리
+//
+//    @ManyToMany(mappedBy = "products" ) //역방향 추가
+//    private List<Chap6_MTM_Member> members = new ArrayList<>();
 
 
 }
